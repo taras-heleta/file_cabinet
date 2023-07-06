bb1658fd23d2a2d55a7f0e208128705eb2b97852
Taras Heleta <taras@Tarass-MacBook-Air.local>
2023-07-06 13:04:53 +0300

    doc controllers and routes

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,31 @@
+class DocsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+  def update
+
+  end
+
+  private
+
+  def find_doc
+
+  end
+
+  def doc_params
+    
+  end
+end
