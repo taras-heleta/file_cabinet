b56320c9a13aebf380f8956de7a8da6121f0fc17
Taras Heleta <taras@Tarass-MacBook-Air.local>
2023-07-10 10:33:23 +0300

    add relation user and docs

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_09_162932) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_09_163416) do
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
